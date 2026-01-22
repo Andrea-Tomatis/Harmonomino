33fcd7476efb37c5992d25e34e38f402dca36d03
Ezra Cerpac <e.cerpac@icloud.com>
2026-01-22 14:45:42 +0100

    implement ef06_max_well_depth and ef07_sum_of_wells (NOT CHECKED)

--- src/eval_fns/ef06_max_well_depth.rs
+++ src/eval_fns/ef06_max_well_depth.rs
@@ -5,6 +5,30 @@ pub struct MaxWellDepth;
 
 impl EvalFn for MaxWellDepth {
     fn eval(&self, board: &Board) -> u8 {
-        todo!()
+        (0..Board::WIDTH)
+            .map(|col| calculate_well_depth(board, col))
+            .max()
+            .unwrap_or(0)
     }
+}
+
+#[must_use]
+pub fn calculate_well_depth(board: &Board, col: usize) -> u8 {
+    let mut depth = 0;
+    for row in 0..Board::HEIGHT {
+        if board[row][col] || board.has_filled_above(row, col) {
+            continue;
+        }
+        // TODO: check if well is allowed to be at edge of the board (I think so)
+        let left_filled = if col > 0 { board[row][col - 1] } else { true };
+        let right_filled = if col < Board::WIDTH - 1 {
+            board[row][col + 1]
+        } else {
+            true
+        };
+        if left_filled && right_filled {
+            depth += 1;
+        }
+    }
+    depth
 }
--- src/eval_fns/ef07_sum_of_wells.rs
+++ src/eval_fns/ef07_sum_of_wells.rs
@@ -1,10 +1,12 @@
-use crate::eval_fns::EvalFn;
+use crate::eval_fns::{EvalFn, ef06_max_well_depth::calculate_well_depth};
 use crate::game::Board;
 
 pub struct SumOfWells;
 
 impl EvalFn for SumOfWells {
     fn eval(&self, board: &Board) -> u8 {
-        todo!()
+        (0..Board::WIDTH)
+            .map(|col| calculate_well_depth(board, col))
+            .sum()
     }
 }
